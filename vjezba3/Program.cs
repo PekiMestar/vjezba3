f66f7b8b33298ce6bd1abd49abaa2c1a18a53cad
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-09-20 09:32:55 +0200

    Add files via upload

---
+++ vjezba3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            Console.Write("Unesite prvu katetu stranicu pravokutnog trokuta: ");
+            x = int .Parse(Console.ReadLine());
+
+            Console.Write("Unesite drugu katetu stranicu pravokutnog trokuta: ");
+            y = int.Parse(Console.ReadLine());
+
+            Console.Write("Unesite hipotenuzu pravokutnog trokuta: ");
+            z = int.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0 && z > 0)
+            {
+
+                if (x * x + y * y == z * z)
+                {
+                    Console.WriteLine("Trokut je pravokutan!");
+                }
+
+                else
+                {
+                    Console.WriteLine("Trokut nije pravokutan!");
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
